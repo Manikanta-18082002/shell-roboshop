136e68a5e7abfcaeee5b907a3180c13004405183
Manikanta-18082002 <konugantimanikantareddy@gmail.com>
2025-10-16 05:47:26 +0530

    Added New Code

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,7 +3,7 @@
 instances=("mongodb" "redis" "mysql" "rabbitmq" "cart" "catalouge" "user" "shipping" "web" "payment")
 
 for names in ${instances[@]}; do
-    if [ $names == "shipping"] || [ $names == "mysql" ]
+    if [ $names == "shipping" ] || [ $names == "mysql" ]
     then 
         instance_type="t3.medium"
     else
