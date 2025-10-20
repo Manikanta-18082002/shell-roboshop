7fd5e0e66a5b758bc90dd0af80bbf1908d3501d2
Manikanta-18082002 <konugantimanikantareddy@gmail.com>
2025-10-20 06:34:20 +0530

    Added New Code

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,6 +11,7 @@ for name in ${instances[@]}; do
     else
         instance_type="t3.micro"
     fi
+    
     echo "Creating instance for: $name with instance type: $instance_type"
     instance_id=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type $instance_type --security-group-ids sg-012dd7c404cb8524d --subnet-id subnet-007b7e3b8abb1e65a --query 'Instances[0].InstanceId' --output text)
     echo "Instance created for: $name"
