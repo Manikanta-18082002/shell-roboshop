c12810b4473cf8b7124eb7ed9bce99574babf697
Manikanta-18082002 <konugantimanikantareddy@gmail.com>
2025-10-21 17:39:29 +0530

    Added New Code

--- catalouge.sh
+++ catalouge.sh
@@ -8,6 +8,8 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
@@ -20,62 +22,67 @@ fi
 
 VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2...$R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2...$G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf module disable nodejs -y &>> $LOG_FILE
-VALIDATE $? "Disable nodejs"
+##### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
 
-dnf module enable nodejs:20 -y &>> $LOG_FILE
-VALIDATE $? "Enable nodejs"
-
-dnf install nodejs -y &>> $LOG_FILE
-VALIDATE $? "Install nodejs"
-
-id roboshop &>> $LOG_FILE
-if [ $? -ne 0]
-then 
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo -e "User already exists... $Y SKIPPING $N"
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
 
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-mkdir /app &>> $LOG_FILE
-VALIDATE $? "Creating /app directory"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue application"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOG_FILE
-VALIDATE $? "Downloading Catalouge application"
+cd /app 
+VALIDATE $? "Changing to app directory"
 
-cd /app  &>> $LOG_FILE
-VALIDATE $? "Moving into /app directory"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-unzip /tmp/catalogue.zip &>> $LOG_FILE
-VALIDATE $? "Extracting Catalouge"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip catalogue"
 
-npm install  &>> $LOG_FILE
-VALIDATE $? "Installing dependencies"
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
 
-cp catalouge.service /etc/systemd/system/catalogue.service &>> $LOG_FILE
-VALIDATE $? "Copying Systemctl services"
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Copy systemctl service"
 
-systemctl daemon-reload &>> $LOG_FILE
-systemctl enable catalogue  &>> $LOG_FILE
-VALIDATE $? "Enabling catalouge"
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
 
-systemctl start catalogue &>> $LOG_FILE
-VALIDATE $? "Start catalouge"
-
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOG_FILE
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copy mongo repo"
 
-dnf install mongodb-mongosh -y &>> $LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
-mongosh --host mongodb.dawsmani.site </app/db/master-data.js &>> $LOG_FILE
+INDEX=$(mongosh mongodb.dawsmani.site --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
 
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
